7491a7bd9f86bee3809df48ad2e5a1d67d49ebd3
Gitaansh <112197932+Gitaanshhh@users.noreply.github.com>
2024-09-16 13:12:58 +0530

    Added Q

--- SEM 3/DS_LAB/Week7/Q3.c
+++ SEM 3/DS_LAB/Week7/Q3.c
@@ -1,3 +1,25 @@
+/*
+Vignesh and his wife Lata are facing a cash crisis. They go to the nearby ATM to get 
+some cash. There are 3 ATMs inside the same room. People are standing in queue 
+outside, and go inside the room in groups of 3 to the ATMs, fetch their money and 
+come out. Lata has an irrational fear in getting money from ATM that her ATM pin 
+will somehow be stolen and her money will be lost. So she will always like to go into 
+room with Vignesh. Vignesh is standing at position K in line, immediately followed 
+by lata (at position K+1). Can you tell whether Vignesh and Lata both will be able to 
+get money in such a way that Lata does not feel insecure? Using queue, find whether 
+they can get money for the given set of N and K. Input: the first line contains an 
+integer T denoting the number of test cases. T test cases follow. The only line of each 
+test case contains two spaces separated integers N and K , Where N denotes number 
+of people in the queue. And K denotes the position of Vignesh . Constraints 
+1<=T<=100 3<=N<=100 1<=K<=N-1, N is divisible by 3.
+Sample Input
+2
+3 1
+6 3
+Output
+Yes 
+No
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
