b178f9c218036241eaa67aa72c1fc45007745902
Gitaansh <112197932+Gitaanshhh@users.noreply.github.com>
2024-10-29 08:27:11 +0530

    Update Q3BTPostFixBuildEval.c

--- SEM 3/DS_LAB/Week11/Q3BTPostFixBuildEval.c
+++ SEM 3/DS_LAB/Week11/Q3BTPostFixBuildEval.c
@@ -4,35 +4,114 @@ Q3 Create an expression tree for the given postfix expression and evaluate it.
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct btree {
-	char data;
+	char ele;
 	struct btree *rchild, *lchild;
 } btree;
 
 typedef struct {
-	int tos;
-	btree elements[20];
-} stack;
+    btree * nodes[100];
+    int tos;
+} Stack;
 
-void push(stack *s, btree ele){
-	s->elements[++(s->tos)]=ele;
+void push(Stack * s, btree * x) {
+    s->nodes[++(s->tos)] = x;
 }
 
-btree pop(stack *s){
-	//free(s->elements[s->tos--]);
-	return s->elements[s->tos];
+btree * pop(Stack * s) {
+    return (s->nodes[(s->tos)--]);
 }
 
+btree * createNode(int x) {
+    btree * new = (btree *) malloc(sizeof(btree));
+    new->ele = x;
+    new->lchild = NULL;
+    new->rchild = NULL;
+    return new;
+}
+
+double eval(char op,double x,double y) {
+    if (op == '+')  return x+y;
+    if (op == '-')  return x-y;
+    if (op == '*')  return x*y;
+    if (op == '/')  return x/y;
+}
+
+int isOperator(char x) {
+    return (x == '+' || x == '-' || x == '*' || x == '/');
+}
+
+double evaluate(btree * node) {
+    if (node == NULL)
+        return 0;
+    if (node->rchild == NULL && node->lchild == NULL)
+        return (double) (node->ele -'0');
+
+    double x = evaluate(node->lchild);
+    double y = evaluate(node->rchild);
+
+    return eval(node->ele,x,y);
+}
+
+btree *createRecursive(char data){
+	char dummy;
+	if (data!='#'){
+		btree *newNode = createNode(data);	
+		printf("L Child Data : ");
+		scanf("%c", &dummy);
+		scanf("%c", &data);
+		newNode->lchild = createRecursive(data);
+		printf("R Child Data : ");
+		scanf("%c", &dummy);
+		scanf("%c", &data);
+		newNode->rchild = createRecursive(data);
+		return newNode;
+	} 
+	return NULL;
+}
+
+void inorder(btree *root){
+	if(root){
+		inorder(root->lchild);
+		printf("%c ", root->ele);
+		inorder(root->rchild);
+	}
+}
+
+int main() {
+    Stack nodeStack;
+    nodeStack.tos = -1;
+    char postfix[100];
+    printf("Enter postfix expression : ");
+    scanf("%s",postfix);
+    int i;
+    for (i=0;i<strlen(postfix);i++) {
+        if (!isOperator(postfix[i]))
+            push(&nodeStack,createNode(postfix[i]));
+        else {
+            btree * root = createNode(postfix[i]);
+            root->rchild = pop(&nodeStack);
+            root->lchild = pop(&nodeStack);
+            push(&nodeStack,root);
+        }
+    }
+
+    btree * root = pop(&nodeStack);
+    printf("Ans :  %lf",evaluate(root));
+}
+
+/*
 btree *postfix_build(char postfix[50]){
-	stack *s = calloc(1, sizeof(stack));
+	Stack *s = calloc(1, sizeof(stack));
 	btree *temp = calloc(1, sizeof(btree));
 	temp->rchild = temp->lchild = NULL;
 	char symbol;
 	int i = 0;
 	while(symbol = postfix[i] != '\0'){
 		if ((symbol>='A' && symbol<='Z')||(symbol>='a' && symbol<='z')||(symbol>='0' && symbol<='9')){
-			temp->data = symbol;
+			temp->ele = symbol;
 			push(s, *temp);
 		}
 		else {
@@ -50,27 +129,4 @@ btree *postfix_build(char postfix[50]){
 	*temp3 = pop(s);
 	return temp3;
 }
-
-void inorder(btree *root){
-	if(root){
-		inorder(root->lchild);
-		printf("%c ", root->data);
-		inorder(root->rchild);
-	}
-}
-
-int main(){
-	char postfix[50];
-	printf("Expression : ");
-	scanf("%s",postfix);
-	btree *root = NULL;
-	root = postfix_build(postfix);
-
-	printf("Inorder : ");
-	inorder(root);
-	printf("\n");
-
-	// int result = eval(root);
-	// printf("Result : %d", result);
-	return 0;
-}
+*/
