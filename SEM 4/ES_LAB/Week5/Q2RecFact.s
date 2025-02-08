eb0718b086bcbe2f62ad27b8a675931f907fe16a
Gitaansh <112197932+Gitaanshhh@users.noreply.github.com>
2025-02-08 13:04:09 +0530

    Added comments

--- SEM 4/ES_LAB/Week5/Q2RecFact.s
+++ SEM 4/ES_LAB/Week5/Q2RecFact.s
@@ -11,30 +11,30 @@ __Vectors
 Reset_Handler
 	LDR R0, =N
 	LDR R1, [R0]
-	MOV R2, #1
+	MOV R2, #1	;r2->fact
 
 recur	
-	BL fact
-	CMP R1, #0
+	BL fact		;call the fact fn
+	CMP R1, #0	; if r1 (or n) = 0, exit loop, else continue recur
 	BEQ exit
 	B recur
 
 exit
-	LDR R5, =FACT
+	LDR R5, =FACT	;Load Fact address and store value then stop	
 	STR R2, [R5]
 	B stop
 
 fact
-	MUL R2, R2, R1
-	SUB R1, #1
-	CMP R1, #0
+	MUL R2, R2, R1		; R2 = R2*R! -> R1 = N
+	SUB R1, #1	; N-=1
+	CMP R1, #0	; if n==0 return else call fact (recursion)
 	BNE fact
 	BX LR
 
 stop
 	B stop
 
-N DCD 3
+N DCD 3		;N = 3
 	AREA mydata, DATA, READWRITE
-FACT DCD 1
+FACT DCD 1	;Fact = 1
 	END
