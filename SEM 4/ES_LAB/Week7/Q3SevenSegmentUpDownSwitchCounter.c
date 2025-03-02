800fb7afe9e17a559cdc6a427449a3d26d33bc9d
Gitaansh <112197932+Gitaanshhh@users.noreply.github.com>
2025-03-02 15:34:38 +0530

    Fixed Switch Selection and configuration. Yet to be Tested

--- SEM 4/ES_LAB/Week7/Q3SevenSegmentUpDownSwitchCounter.c
+++ SEM 4/ES_LAB/Week7/Q3SevenSegmentUpDownSwitchCounter.c
@@ -7,7 +7,6 @@ unsigned char tohex[10] = {0x3F, 0X06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F,
 unsigned int i = 0, j = 0, temp, counter=9999, switchState=1, flag = 1;
 
 // Array to store the digits for each of the four 7-segment display positions
-long int arr[4] = {9, 9, 9, 9}; // Starts at 9999, representing a four-digit countdown
 
 void initTimer0(void){
 	LPC_SC->PCONP |= (1<<1); //Power up TIM0
@@ -32,17 +31,18 @@ int main(){
     SystemInit();
     SystemCoreClockUpdate();
   
-		initTimer0();
-	
-		LPC_PINCON->PINSEL4 &= 0xFCFFFFFF; //For Key
-		LPC_GPIO2->FIODIR &= ~(1<<12);//0x00001000;
+    initTimer0();
+
+    //Using CND for key => 7th pin is P2.0
+    LPC_PINCON->PINSEL4 &= 0xFFFFFFFC; //For Key
+    LPC_GPIO2->FIODIR &= 0xFFFFFFFE //~0x00000001 
 	
     // Configure GPIO pins for the 7-segment displays
     // LPC_GPIO0 pins 0 to 7 are set as outputs to control the 7-segment display (8 bits)
     LPC_GPIO0->FIODIR |= 0xFF0;  // Set bits 4 to 11 as output (8 bits for 7-segment display)
     
     // LPC_GPIO1 pins 23 to 26 are set as outputs to select which 7-segment digit to control
-    LPC_GPIO1->FIODIR |= 0xF << 23; // Set bits 23-26 as output (4 bits for digit select)
+    LPC_GPIO1->FIODIR |= 0xF << 23; // Set bits 23-26 as output (4 bits for digit select) - Not sure why yet, mux => 23 and 24th bit only
 
 		while (1){
 			
