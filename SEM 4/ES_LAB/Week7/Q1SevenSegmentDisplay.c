6b26cde82542edf25875b815659088907f9d44f1
Gitaansh <112197932+Gitaanshhh@users.noreply.github.com>
2025-03-02 15:23:13 +0530

    Fixed
    Assumed GPIO1 FIOPIN to act in one hot encoded form. It actually works like a mux.

--- SEM 4/ES_LAB/Week7/Q1SevenSegmentDisplay.c
+++ SEM 4/ES_LAB/Week7/Q1SevenSegmentDisplay.c
@@ -22,7 +22,7 @@ int main() {
     while(1) {
         for(i = 0; i < 4; i++) {
             // Select the current digit (activate the appropriate digit by shifting 1 to the correct bit)
-            LPC_GPIO1->FIOPIN = (1 << (i + 23 - 1));  // Activate one of the four digits (0-3)
+            LPC_GPIO1->FIOPIN = (i << 23);  // Activate one of the four digits (0-3), behaves like a mux - 00, 01, 10, 11 (NOT one hot encoded)
             
             // Send the corresponding 7-segment pattern for the current digit
             LPC_GPIO0->FIOPIN = tohex[digits[i]] << 4;  // Shift the 7-segment pattern for the current digit
