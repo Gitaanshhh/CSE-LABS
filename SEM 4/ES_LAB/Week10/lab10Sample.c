8b4a1ba450ce0c90808f73d4ed0d5c90a6632d01
Gitaansh <112197932+Gitaanshhh@users.noreply.github.com>
2025-03-19 11:11:20 +0530

    Fixed display alignment

--- SEM 4/ES_LAB/Week10/lab10Sample.c
+++ SEM 4/ES_LAB/Week10/lab10Sample.c
@@ -54,11 +54,11 @@ int main(void) {
 		
 		for(i=0; i<2000; i++); 
 
-		lcd_comdata(0x89, 0); 
+		lcd_comdata(0x8B, 0); 
 		delay_lcd(800); 
 		lcd_puts(&vtg[0]); 
 
-		lcd_comdata(0xC8, 0); 
+		lcd_comdata(0xCC, 0); 
 		delay_lcd(800); 
 		lcd_puts(&dval[0]);
 		
