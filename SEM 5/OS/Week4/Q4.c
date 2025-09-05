ddebe85289521a9f48e68422625357eb0214e340
Gitaansh <112197932+Gitaanshhh@users.noreply.github.com>
2025-09-05 16:33:33 +0530

    Add files via upload

---
+++ SEM 5/OS/Week4/Q4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int a = 81;
+    float b = 9.1;
+    char c = 'p';
+    char str[] = "OS lab";
+    printf("a = %d b = %f c = %c str = %s\nHexadecimal a=%x", a, b, c, str, a);
+    return 0;
+}
